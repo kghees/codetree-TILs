a002d55a8363527dcafd436aad1c3cea65710b69
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-21 15:19:36 +0900

    Title: 변수 출력하기, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

--- 231121/변수 출력하기/outputing-variables.java
+++ 231121/변수 출력하기/outputing-variables.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int a = 3;
-        int b = 'C';
-        System.out.printf("%d...%c",a,b);
+        char b = 'C';
+        System.out.print(a + "..." + b);
     }
 }
