2c7e003e6fc7793f42d1d8670e3aee7c73ccb07f
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-22 22:30:15 +0900

    Title: 369 게임, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 231122/369 게임/369-game.java
+++ 231122/369 게임/369-game.java
@@ -4,7 +4,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         for(int i = 1; i <= n; i++){
-            if(i % 3 == 0 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
+            if(i % 3 == 0)
+                System.out.print(0 + " ");
+            else if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9)
+                System.out.print(0 + " ");
+            else if(i / 10 == 3 || i / 10 == 6 || i / 10 \\ 9)
                 System.out.print(0 + " ");
             else
                 System.out.print(i + " ");
