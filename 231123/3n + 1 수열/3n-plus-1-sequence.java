0fe48004de5eab938b2cdf418cf4c745e2abdc5b
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-23 19:22:43 +0900

    Title: 3n + 1 수열, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231123/3n + 1 수열/3n-plus-1-sequence.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+        while(true){
+            if(n==1){
+                break;
+            }
+            if(n % 2 == 1)
+                n = 3 * n + 1;
+            else
+                n /= 2;
+            cnt++;
+        }
+        System.out.print(cnt);
+    }
+}
