d4d060bf9d350d9b5eaefac61c94fa295d2bf746
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-23 19:51:27 +0900

    Title: 공약수의 유무, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 231123/공약수의 유무/presence-or-absence-of-a-common-divisor.java
+++ 231123/공약수의 유무/presence-or-absence-of-a-common-divisor.java
@@ -6,7 +6,7 @@ public class Main {
         int b = sc.nextInt();
         boolean check = false;
         for(int i = a; i <= b; i++){
-            if(1920 % i == 0 || 2880 % i == 0){
+            if(1920 % i == 0 && 2880 % i == 0){
                 check = true;
             }
         }
