466fa1cf4c0f4efb5ca27bd932a164181229b8bd
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-23 18:54:29 +0900

    Title: 1부터의 합, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231123/1부터의 합/sum-from-the-one.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        int cnt = 0;
+        for(int i = 1; i <= 100; i++){
+            sum += i;
+            if(sum >= n){
+                cnt = i;
+                break;
+            }
+        }
+        System.out.print(cnt);
+    }
+}
