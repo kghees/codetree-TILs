13307a7fecdb9d22250075edb92801c835e57ff7
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-25 22:38:03 +0900

    Title: 100 도달하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 231125/100 도달하기/reach-100.java
+++ 231125/100 도달하기/reach-100.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[10];
+        int[] arr = new int[100];
         int n = sc.nextInt();
         arr[0] = 1;
         arr[1] = n;
