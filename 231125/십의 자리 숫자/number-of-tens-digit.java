a10b39d1ac394349120772316e2d2dceb660bd5b
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-25 22:49:51 +0900

    Title: 십의 자리 숫자, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231125/십의 자리 숫자/number-of-tens-digit.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        while(true){
+            int n = sc.nextInt();
+            if(n == 0)
+                break;
+            if(n >= 10)
+                arr[n / 10]++;
+        }
+        for(int i = 1; i < 10; i++){
+            System.out.println(i + " - " + arr[i]);
+        }
+    }
+}
