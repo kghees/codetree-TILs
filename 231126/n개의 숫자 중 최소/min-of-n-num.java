c2dce9b153493a91447779a0026f51148376495f
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-26 15:53:16 +0900

    Title: n개의 숫자 중 최소, Time: 172ms, Memory: 10MB, Status: Passed - CodeTree

--- 231126/n개의 숫자 중 최소/min-of-n-num.java
+++ 231126/n개의 숫자 중 최소/min-of-n-num.java
@@ -6,14 +6,14 @@ public class Main {
         int x = Integer.MAX_VALUE, y = Integer.MIN_VALUE;
         int cnt = 0, cnt1 = 0;
         int[] a = new int[n];
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < n; i++){
             a[i] = sc.nextInt();
             if(a[i] < x)
                 x = a[i];
             if(a[i] > y)
                 y = a[i];
         }
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < n; i++){
             if(a[i] == x)
                 cnt++;
             if(a[i] == y)
