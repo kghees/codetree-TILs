e4ae604953dc30b3e91622462b092dd68142dd3d
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-26 17:12:48 +0900

    Title: 숫자 직사각형, Time: 202ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 231126/숫자 직사각형/rectangle-of-numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] a = new int[n][m];
+        int x = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                a[i][j] = x;
+                x += 1;
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
