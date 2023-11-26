c0940a84b28f4c1e377b0ebe93809bc9421f1e16
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-26 18:02:39 +0900

    Title: 동전이 있는 위치, Time: 175ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231126/동전이 있는 위치/Where-coins-are-located.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] a = new int[n][n];
+        for(int i = 0; i < m; i++){
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            a[x-1][y-1] = 1;
+        }
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
