fd2f7a55a5d32615f32d46d358d12f28908cb764
김건희 <92205960+kghees@users.noreply.github.com>
2023-11-26 20:30:16 +0900

    Title: 문자열의 총 길이 구하기, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231126/문자열의 총 길이 구하기/find-the-length-of-the-string.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] a = new String[10];
+        int sum = 0;
+        for(int i = 0; i < 10; i++){
+            a[i] = sc.next();
+            sum += a[i].length();
+        }
+        System.out.print(sum);
+    }
+}
