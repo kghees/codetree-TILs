e7d5d0b712118b3c371f6aed794e87689f600140
김건희 <92205960+kghees@users.noreply.github.com>
2023-12-27 14:23:35 +0900

    Title: 중앙값 구하기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/중앙값 구하기/find-the-median.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a > b){
+        if(b > c)
+            printf("%d",b);
+        else if(c > a)
+            printf("%d",a);
+        else
+            printf("%d",c);
+    }
+   else{
+    if(c > b)
+        printf("%d",b);
+    else if(a > c)
+        printf("%d",a);
+    else
+        printf("%d",c);
+   }
+    return 0;
+}
