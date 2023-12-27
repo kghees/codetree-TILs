bb77aa1c455fc70f71ba77f5ff0a59950556b0ff
김건희 <92205960+kghees@users.noreply.github.com>
2023-12-27 13:48:35 +0900

    Title: 물의 상태, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/물의 상태/state-of-water.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    scanf("%d",&a);
+    if(a < 0)
+        printf("ice");
+    else if(a >= 100)
+        printf("vapor");
+    else
+        printf("water");
+    return 0;
+}
