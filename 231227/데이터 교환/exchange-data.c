3caf27933736440e2f6fce8c11c4783b6bfaa7b2
김건희 <92205960+kghees@users.noreply.github.com>
2023-12-27 11:22:34 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231227/데이터 교환/exchange-data.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 5;
+    int b = 6;
+    int c = 7;
+    int temp = a;
+    a = c;
+    c = b;
+    b = temp;
+    printf("%d\n%d\n%d",a,b,c);
+    return 0;
+}
