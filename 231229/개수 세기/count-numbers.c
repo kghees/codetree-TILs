0df3b84ed9bef7375737a374b4bdf830a7dfd7e1
김건희 <92205960+kghees@users.noreply.github.com>
2023-12-29 15:07:28 +0900

    Title: 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 231229/개수 세기/count-numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    scanf("%d %d",&n,&m);
+    int arr[100];
+    int cnt = 0;
+    for(int i = 0; i < n; i++){
+        scanf("%d",&arr[i]);
+        if(arr[i] == m)
+            cnt++;
+    }
+    printf("%d", cnt);
+    return 0;
+}
