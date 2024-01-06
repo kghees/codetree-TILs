3e2c78970cd35e00a777fa8ca50bf51c0801ee92
김건희 <92205960+kghees@users.noreply.github.com>
2024-01-06 14:59:52 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240106/데이터 교환/exchange-data.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 5;
+    int b = 6;
+    int c = 7;
+    int temp;
+    temp = a;
+    a = c;
+    c = b;
+    b = temp;
+    cout << a << endl << b << endl << c;
+    return 0;
+}
