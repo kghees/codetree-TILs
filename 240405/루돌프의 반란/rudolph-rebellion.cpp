615e3f0bb05e269b649c3b86726b9da95f7eaac8
김건희 <92205960+kghees@users.noreply.github.com>
2024-04-05 22:16:06 +0900

    Title: 루돌프의 반란, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240405/루돌프의 반란/rudolph-rebellion.cpp
+++ 240405/루돌프의 반란/rudolph-rebellion.cpp
@@ -6,22 +6,19 @@ using namespace std;
 
 int n, m, p, c, d;
 int ry, rx;
-int map[51][51] = { 0 };
+int map[52][52] = { 0 };
 int dy[4] = { -1,0,1,0 }; //0123 상우하좌
 int dx[4] = { 0,1,0,-1 };
-bool santa_live[50] = { false }; //산타가 살아 있는지
-pair<int, int> santa_location[60]; //번호에 따른 산타 좌표 
-int final_score[31];
+bool santa_live[32] = { false }; //산타가 살아 있는지
+pair<int, int> santa_location[32]; //번호에 따른 산타 좌표 
+int final_score[32];
 int stun_santa[1004];
 
 //연쇄충돌 확인 및 일반충돌 처리 
 //c만큼 밀린 y,x좌표, 해당 방향으로 1밀리기 위한 y,x 현재 산타 번호
 void crash(int cy, int cx, int my, int mx, int s_num) {
-    //연쇄 충돌
     //밀려나기 전 산타번호
     int idx = map[cy][cx];
-    int idxy = santa_location[cy].first;
-    int idxx = santa_location[cx].second;
     map[cy][cx] = s_num;
     santa_location[s_num] = { cy,cx };
 
@@ -81,8 +78,8 @@ void move_dear(int y, int x, int turn) {
     }
 
     //가까운 산타 찾았으므로 루돌프 이동 시키기
-    //루돌프 이동하면 이동하기 전 루돌프 위치 0으로 만들어주기 위함
-    //가까운 산타가 존재하면 움직임 시작 없으면 끝내야함
+    
+    //가까운 산타가 존재하면 움직임 시작 없으면 살아있는 산타 X이므로 끝내야함
     if (santa_num) {
         int tempry = ry, temprx = rx;
         int movey, movex;
@@ -99,7 +96,6 @@ void move_dear(int y, int x, int turn) {
         map[tempry][temprx] = 0;
         //루돌프 이동 했는데 산타가 있을 경우
         if (same_distance.first == ry && same_distance.second == rx) {
-            final_score[santa_num] += c; //해당하는 산타 번호에 c만큼 점수 더해주기
             //스턴
             //산타 움직일때 turn+1보다 크면 다시 움직일 수 있음
             stun_santa[santa_num] = turn + 1;
@@ -107,6 +103,7 @@ void move_dear(int y, int x, int turn) {
             int tempx = same_distance.second + movex * c;
             //범위 넘어가면
             if (tempy > n || tempy < 1 || tempx > n || tempx < 1) {
+                final_score[santa_num] += c;
                 santa_live[santa_num] = false; //산타 탈락
                 map[same_distance.first][same_distance.second] = 0; //탈락한 산타 위치 초기화
                 santa_location[santa_num] = { 0,0 }; //탈락한 산타 위치 초기화
@@ -118,6 +115,7 @@ void move_dear(int y, int x, int turn) {
                 //연쇄충돌 처리
                 //c만큼 밀린 y,x좌표, 해당 방향으로 1밀리기 위한 y,x 현재 산타 번호
                 crash(tempy, tempx, movey, movex, santa_num);
+                
             }
             //일반충돌
             else {
@@ -125,6 +123,7 @@ void move_dear(int y, int x, int turn) {
                 map[tempy][tempx] = santa_num;
                 santa_location[santa_num] = { tempy,tempx };
             }
+            final_score[santa_num] += c; //해당하는 산타 번호에 c만큼 점수 더해주기
         }
     }
     map[ry][rx] = -1;
@@ -187,7 +186,9 @@ void move_santa(int turn) {
                     else {
                         //연쇄 충돌
                         if (map[tempcy][tempcx] > 0) {
+                            map[santa_location[i].first][santa_location[i].second] = 0;
                             crash(tempcy,tempcx,-dy[flag],-dx[flag],i);
+                            
                         }
                         //일반 충돌
                         else {
@@ -221,14 +222,12 @@ int main() {
         santa_location[num] = { y,x };
         santa_live[num] = true;
     }
-    int de = 1;
     for (int i = 1; i <= m; i++) {
         move_dear(ry, rx, i);
 
         move_santa(i);
 
         bool flag = false;
-        int cnt = 0;
         //살아 남은 산타들 1점씩 추가
         //살아 남은 산타가 있는지 확인
         for (int i = 1; i <= p; i++) {
